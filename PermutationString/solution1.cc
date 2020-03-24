7fec261eb2777385448b32be9cfa603e93f5d4a7
Jiajun Guo <j-guo@Jiajuns-iMac.local>
2020-03-24 22:41:25 +0900

    Permutation String

--- PermutationString/solution1.cc
+++ PermutationString/solution1.cc
@@ -11,15 +11,9 @@ bool isPermutation(std::string str1, std::string str2)
 
   std::sort(str1.begin(), str1.end());
   std::sort(str2.begin(), str2.end());
-  int length = str1.size();
-  for(int i = 0; i < length; i++)
-  {
-    if(str1[i] == str2[i])
-      continue;
-    else 
-      return false;
-  }
-  return true;
+  if(str1 == str2)
+    return true;
+  return false;
 }
 
 int main()
